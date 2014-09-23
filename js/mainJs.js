45559136645990499944f4a7dd79b9803316cef0
mdwork <mazdze@gmail.com>
2014-09-23 16:12:49 +0300

    Zheka

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,12 +1,13 @@
 (function($){
     function positionFotoGoods(foto) {
-        var widthFoto = foto.width();
+        var widthFoto = foto.width() / 1.5,
+            heightFoto = foto.height() / 1.5;
+
+        foto.width(widthFoto).height(heightFoto);
         foto.css('marginLeft', -widthFoto / 2);
     }
     var fotoGoods1 = $('.foto-goods-size1'),
         fotoGoods2 = $('.foto-goods-size2');
 
     positionFotoGoods(fotoGoods1);
-    positionFotoGoods(fotoGoods2);
-
 })(jQuery);
