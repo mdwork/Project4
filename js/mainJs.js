02033b8cbd29f309939c3f9b3cd725ecefa69434
mdwork <mazdze@gmail.com>
2014-09-23 16:59:08 +0300

    Zheka

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,10 +1,15 @@
 (function($){
     function positionFotoGoods(foto) {
         for(var i = 0; i < foto.length; i++) {
-            $(foto[i]).width($(foto[i]).width() / 1.5).height($(foto[i]).height());
-            $(foto[i]).css('marginLeft', -$(foto[i]).width() / 2);
+            var curForo = $(foto[i]),
+                curFotoWidth = $(foto[i]).width(),
+                curFotoHeight = $(foto[i]).height();
+
+            curForo.width(curFotoWidth / 1.5).height(curFotoHeight / 1.5);
+            curForo.css('marginLeft', -curFotoWidth / 3 + 'px');
         }
     }
+
     var fotoGoods = $('.foto-goods-size1'),
         fotoNav = $('.box-catalog img');
 
