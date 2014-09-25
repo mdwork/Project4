80e2ee282bd0a249e2f7f5cc13f83db12a1ac168
mdwork <mazdze@gmail.com>
2014-09-25 17:10:37 +0300

    Ihor 25-09-2014

--- js/mainJs.js
+++ js/mainJs.js
@@ -2,21 +2,15 @@ $(document).ready(function(){
     function positionFotoGoods(foto) {
         $(foto).each(function(){
             var curForo = $(this),
-                curFotoWidth = $(this).width(),
-                curFotoHeight = $(this).height();
+                curFotoWidth = $(this).width();
 
-            console.log(curFotoWidth);
-            curForo.width(curFotoWidth / 1.5).height(curFotoHeight / 1.5);
-            curForo.css('marginLeft', -curFotoWidth / 3 + 'px');
+            curForo.css('marginLeft', -curFotoWidth / 2 + 'px');
         });
-
     }
 
-    var fotoGoods = $('.foto-goods-size1'),
-        fotoGoods2 = $('.foto-goods-size2'),
+    var fotoGoods = $('.box-foto-goods img'),
         fotoNav = $('.box-catalog img');
 
     positionFotoGoods(fotoGoods);
-    positionFotoGoods(fotoGoods2);
     positionFotoGoods(fotoNav);
 });
